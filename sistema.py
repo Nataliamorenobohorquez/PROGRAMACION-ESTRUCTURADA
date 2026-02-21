fda97b0c6c9bd37c14d76fe8aff8b07f3d56dd39
Natalia Moreno <Nmoreno220805@gmail.com>
2026-02-21 16:06:16 -0400

    Primer commit - Sistema de Ventas

---
+++ sistema.py
@@ -0,0 +1,15 @@
+# Nombre: Natalia Moreno
+# Proyecto: Sistema de Ventas
+
+# Variables iniciales
+producto = "Cuaderno"
+precio = 15
+cantidad = 3
+
+# Operación básica
+total = precio * cantidad
+
+# Saludo al usuario
+print("Bienvenida al Sistema de Ventas")
+print("Producto:", producto)
+print("Total a pagar:", total)
